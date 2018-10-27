65dc38bb448b8fd664e7cfb2781643eb3e64e842
Emilio Lopez <eil11@case.edu>
2018-10-27 11:54:33 -0400

    Start using configparser for api keys

--- cardinal/main.py
+++ cardinal/main.py
@@ -1,4 +1,7 @@
-import requests
+import configparser
 
 if __name__ == '__main__':
-    print("hello")
+    config = configparser.ConfigParser()
+    config.read('api.ini')    # not in the git repo
+
+    owm_api = config.get('API', 'OWM')
