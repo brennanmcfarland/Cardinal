8d64f83fcdf3e88a282a8dfeb00399a71861b2cd
Emilio Lopez <eil11@case.edu>
2018-10-27 15:54:16 -0400

    Break different requests for cardinal into methods in main

--- cardinal/main.py
+++ cardinal/main.py
@@ -2,13 +2,21 @@ import configparser
 from cardinal.weather import weather_info
 from cardinal.news import news_headlines
 
-if __name__ == '__main__':
-    config = configparser.ConfigParser()
-    config.read('api.ini')    # not in the git repo
+config = configparser.ConfigParser()
+config.read('api.ini')  # not in the git repo
 
-    owm_api = config.get('API', 'OWM')
-    news_api = config.get('API', 'NEWS')
+owm_api = config.get('API', 'OWM')
+news_api = config.get('API', 'NEWS')
 
+
+def get_weather_info():
     print(weather_info('Dallas', owm_api))
+
+
+def get_news_headlines():
     print(news_headlines(news_api))
 
+
+if __name__ == '__main__':
+    get_weather_info()
+    get_news_headlines()
