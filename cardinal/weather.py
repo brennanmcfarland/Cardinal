4949fa9f443f56fdcaa62cddbeebcd97145ca27a
Emilio Lopez <eil11@case.edu>
2018-10-27 11:52:54 -0400

    Create weather module for getting current weather

---
+++ cardinal/weather.py
@@ -0,0 +1,11 @@
+import requests
+
+WEATHER_URL = 'api.openweathermap.org/data/2.5/weather'
+
+
+def get_weather_in_city(city, owm_api):
+    city_string = '?q=' + city
+    app_id = 'APPID=' + owm_api
+    request_url = WEATHER_URL + city_string + '&' + app_id
+
+    return requests.get(request_url).text
