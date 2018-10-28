57614aff8d44c4424925bc862a51340c7f549859
Emilio Lopez <eil11@case.edu>
2018-10-28 02:10:18 -0400

    add text to speech for output

--- cardinal/main.py
+++ cardinal/main.py
@@ -6,29 +6,31 @@ from cardinal.news import news_headlines
 from cardinal.wikipedia import get_wikipedia_page_for_topic
 from cardinal.speech import get_command
 from cardinal.intent import get_intent_from_text
+from cardinal.voice import text_to_speech
 
 config = configparser.ConfigParser()
 config.read('api.ini')  # not in the git repo
 
 owm_api = config.get('API', 'OWM')
 news_api = config.get('API', 'NEWS')
+tts_api = config.get('API', 'SPEECH')
 
 
 def get_weather_info(city):
-    print(weather_info(city, owm_api))
+    return weather_info(city, owm_api)
 
 
 def get_news_headlines():
-    print(news_headlines(news_api))
+    return news_headlines(news_api)
 
 
 def get_wikipedia_page(topic):
-    print(get_wikipedia_page_for_topic(topic))
+    return get_wikipedia_page_for_topic(topic)
 
 
 def get_local_time():
     now = datetime.datetime.now()
-    print(now.strftime("%A, %d of %B, %Y\n"))
+    return now.strftime("%A, %d of %B, %Y\n")
 
 
 if __name__ == '__main__':
@@ -36,14 +38,17 @@ if __name__ == '__main__':
     intent_json = get_intent_from_text(command_text)
     intent = intent_json['topScoringIntent']['intent']
 
+    output = ''
     if intent == 'weather':
-        get_weather_info(intent_json['entities'][0]['entity'])
+        output = get_weather_info(intent_json['entities'][0]['entity'])
     elif intent == 'wikipedia':
-        get_wikipedia_page(intent_json['entities'][0]['entity'])
+        output = get_wikipedia_page(intent_json['entities'][0]['entity'])
     elif intent == 'time':
-        get_local_time()
+        output = get_local_time()
     elif intent == 'news':
-        get_news_headlines()
+        output = get_news_headlines()
     else:
         print('Unknown intent, exiting')
         sys.exit(1)
+
+    text_to_speech(output, tts_api)
