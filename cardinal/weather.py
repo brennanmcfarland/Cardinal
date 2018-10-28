9e08a155c5a35354db6186c54efb4477c0d8519f
Emilio Lopez <eil11@case.edu>
2018-10-28 02:46:17 -0400

    weather opesn web page

--- cardinal/weather.py
+++ cardinal/weather.py
@@ -1,5 +1,6 @@
 import requests
 import decimal
+import webbrowser
 
 WEATHER_URL = 'http://api.openweathermap.org/data/2.5/weather'
 decimal.getcontext().rounding = decimal.ROUND_DOWN
@@ -15,7 +16,8 @@ def __get_weather_request(city, owm_api):
 
 def __parse_fields(response):
     relevant_field_dict = dict(conditions=response['weather'][0]['main'],
-                               temperature=__convert_kelvin_fahrenheit(response['main']['temp']))
+                               temperature=__convert_kelvin_fahrenheit(response['main']['temp']),
+                               id=response['id'])
     return relevant_field_dict
 
 
@@ -29,9 +31,15 @@ def __get_weather_in_city(city, owm_api):
     return __parse_fields(__get_weather_request(city, owm_api))
 
 
+def __open_weather_page(city_id):
+    url = 'https://openweathermap.org/city/' + str(city_id)
+    webbrowser.open_new(url)
+
+
 def weather_info(city, owm_api):
     weather_info_dict = __get_weather_in_city(city, owm_api)
 
+    __open_weather_page(weather_info_dict['id'])
     return (f"The weather in {city} is {weather_info_dict['conditions']}, "
             f"with a temperature of {weather_info_dict['temperature']}\n")
 
